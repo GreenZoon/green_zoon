c410723ebfdbde8e9cc48dc451410cf1f3f83690
narasophiah-dotcom <narasophiah@gmail.com>
2026-07-25 22:36:12 +0900

    js 메인타이틀 갯수 세기 뻄, 두번쨰 섹션 미세작업

--- js/common.js
+++ js/common.js
@@ -20,33 +20,19 @@ async function loadComponent(id, file) {
         const html = await response.text();
 
         console.log("불러온 파일:", response.url);
-        console.log(
-            "원본 main_tit 개수:",
-            (html.match(/class\s*=\s*["'][^"']*\bmain_tit\b[^"']*["']/g) || []).length
-        );
 
         target.innerHTML = html;
-
-        console.log(
-            "삽입 후 DOM main_tit 개수:",
-            target.querySelectorAll(".main_tit").length
-        );
+        
     } catch (error) {
         console.error(error);
     }
 }
-
-loadComponent("header", "./components/header_v2.html");
-loadComponent("footer", "./components/footer.html");
-loadComponent("menu", "./components/menu.html");
-
 
 fetch("./components/header_v2.html?v=" + Date.now(), {
     cache: "no-store"
 })
     .then(response => response.text())
     .then(html => {
-        console.log("새 파일 main_tit:", (html.match(/main_tit/g) || []).length);
         console.log("새 파일 길이:", html.length);
         console.log("COMMON-JS-V2 실행됨");
     });
@@ -113,3 +99,7 @@ function closeHeaderSearch() {
     searchForm?.classList.remove("is_open");
     openButton?.setAttribute("aria-expanded", "false");
 }
+
+loadComponent("header", "./components/header_v2.html");
+loadComponent("footer", "./components/footer.html");
+loadComponent("menu", "./components/menu.html");
